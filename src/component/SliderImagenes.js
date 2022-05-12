9df1e01de8c369951fae3758d648fabda43e142e
Adrian12377 <79296010+Adrian12377@users.noreply.github.com>
2022-05-11 17:03:15 -0700

    Carrusel
    Integradora Diseño De Interfaces | Adrian Keith Duran

---
+++ src/component/SliderImagenes.js
@@ -0,0 +1,53 @@
+import i1 from "../imagenes/1.png";
+import i2 from "../imagenes/2.png";
+import i3 from "../imagenes/3.png";
+import i4 from "../imagenes/4.png";
+import i5 from "../imagenes/5.png";
+import i6 from "../imagenes/6.png";
+import i7 from "../imagenes/7.png";
+import i8 from "../imagenes/8.png";
+import i9 from "../imagenes/9.png";
+import i10 from "../imagenes/10.png";
+import i11 from "../imagenes/11.png";
+import i12 from "../imagenes/12.png";
+
+
+
+export default [
+    {
+        urls: i1,
+    },
+    {
+        urls: i2
+    },
+    {
+        urls: i3
+    },
+    {
+        urls: i4
+    },
+    {
+        urls: i5
+    },
+    {
+        urls: i6
+    },
+    {
+        urls: i7
+    },
+    {
+        urls: i8
+    },
+    {
+        urls: i9
+    },
+    {
+        urls: i10
+    },
+    {
+        urls: i11
+    },
+    {
+        urls: i12
+    },
+];
